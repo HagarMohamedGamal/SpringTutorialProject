3bd6bf5f40683d4dd24ca57568bc794f100c01fa
HagarMohamedGamal <hagar.mohamed.gamal@gmail.com>
2022-09-10 10:09:33 +0200

    initiate the project

---
+++ src/test/java/com/springtutorialproject/SpringTutorialProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtutorialproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutorialProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
