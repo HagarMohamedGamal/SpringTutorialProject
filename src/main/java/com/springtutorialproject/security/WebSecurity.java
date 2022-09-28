198b2556bb63f788cfe46720d079a9b00c804feb
hgamal <hgamal@RD-A1-DT-4011.ISFPDOMAIN.com>
2022-09-28 13:16:53 +0200

    Make the sing-up Web Service Endpoint public

---
+++ src/main/java/com/springtutorialproject/security/WebSecurity.java
@@ -0,0 +1,19 @@
+package com.springtutorialproject.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class WebSecurity {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, "/users").permitAll().anyRequest()
+				.authenticated();
+		return http.build();
+	}
+
+}
