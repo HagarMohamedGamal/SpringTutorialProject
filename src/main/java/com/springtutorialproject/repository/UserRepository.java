fa0b04f8173849fc9bfe16d6f68484ac84870410
HagarMohamedGamal <hagar.mohamed.gamal@gmail.com>
2022-10-16 23:28:16 +0200

    Using native SQL query in different ways 'Paging, positional parameters and named parameters'

--- src/main/java/com/springtutorialproject/repository/UserRepository.java
+++ src/main/java/com/springtutorialproject/repository/UserRepository.java
@@ -1,6 +1,12 @@
 package com.springtutorialproject.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.springtutorialproject.entities.UserEntity;
@@ -9,7 +15,16 @@ import com.springtutorialproject.entities.UserEntity;
 public interface UserRepository extends PagingAndSortingRepository<UserEntity, Long> {
 
 	UserEntity findByEmail(String email);
-	
+
 	UserEntity findByUserId(String userId);
-	
+
+	@Query(value = "select * from Users u where u.email_verification_token = 'false'", countQuery = "select count(*) from Users u where u.email_verification_token = 'false'", nativeQuery = true)
+	Page<UserEntity> findAllusersWithNonConfirmedEmail(Pageable pageableRequest);
+
+	@Query(value = "select * from Users u where u.first_name = ?1", nativeQuery = true)
+	List<UserEntity> findUserByFirstName(String firstName);
+
+	@Query(value = "select * from Users u where u.last_name = :lastName", nativeQuery = true)
+	List<UserEntity> findUserByLastName(@Param("lastName") String lastName);
+
 }
